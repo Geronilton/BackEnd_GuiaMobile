685264cc9c15fec321e0a7adc551b7e1dbf453ab
geronilton <geronilton001@gmail.com>
2025-02-05 21:26:42 -0300

    configurações do BD e Relacionamentos das tabelas

--- config/db.js
+++ config/db.js
@@ -1,18 +1,25 @@
-const { Sequelize } = require('sequelize');
-const User = require('../models/User')
+import { Sequelize } from 'sequelize';
+import Usuarios from '../models/User.js';
+import Rotas from '../models/Rotas.js';
+import Instrucoes from '../models/Instrucoes.js';
+import Rota_Instrucao from '../models/Rotas_Instrucao.js';
 
-const sequelize = new Sequelize('bd_registro', 'root', '123456', {
+const sequelize = new Sequelize('RotasDB', 'root', '123456', {
     host: 'localhost',
     dialect: 'mysql'
 });
 
-try {
-    sequelize.authenticate();
-    console.log("Conexão com o banco de dados estabelicida!!");
-}catch(error) {
-    console.log("Erro ao conectar com o banco de dados!!", error);
-}
+Usuarios.init(sequelize);
+Rotas.init(sequelize);
+Instrucoes.init(sequelize);
+Rota_Instrucao.init(sequelize);
 
-User.init(sequelize);
+// Configurando associações
+Usuarios.associate({ Rotas });
+Rotas.associate({ Usuarios, Rota_Instrucao });
+Instrucoes.associate({ Rota_Instrucao });
+Rota_Instrucao.associate({ Rotas, Instrucoes });
 
-module.exports = sequelize;
+export { Usuarios, Rotas, Instrucoes, Rota_Instrucao };
+
+export default sequelize;
