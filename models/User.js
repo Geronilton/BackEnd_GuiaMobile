8e77d304a938ec102c3fcec1321eeb6a759c7342
geronilton <geronilton001@gmail.com>
2025-02-05 21:22:43 -0300

    Model de usuarios

--- models/User.js
+++ models/User.js
@@ -1,23 +1,26 @@
-const {Model, DataTypes} = require('sequelize');
-const bcrypt = require('bcrypt')
+import { Model,DataTypes } from 'sequelize';
+import bcrypt from 'bcrypt'
 
 class User extends Model {
     static init(sequelize) {
         super.init({
-            name: DataTypes.STRING,
+            nome: DataTypes.STRING,
             email: DataTypes.STRING,
             password: DataTypes.VIRTUAL,
-            password_hash: DataTypes.STRING,
+            senha: DataTypes.STRING,
         },{
             sequelize,
-            modelName: 'user',
+            modelName: 'Usuarios',
         })
-        this.addHook('beforeSave', async user =>{
-            if(user.password){
-                user.password_hash = await bcrypt.hash(user.password, 8)
-            }
-        })
+        this.addHook('beforeSave', async (user) => {
+            if (user.password) {
+                user.senha = await bcrypt.hash(user.password, 8);
+            }}
+        );
+    }
+    static associate(models) {
+        this.hasMany(models.Rotas, { foreignKey: 'usuario_id', as: 'rotas' });
     }
 }
 
-module.exports = User;
+export default User;
